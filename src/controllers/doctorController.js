268bdcd5cc5e8acd4a622050df534f6ed3600953
Perdijk <johanna.perdijkk@gmail.com>
2026-05-14 16:09:31 -0600

    Update doctorController.js

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,11 +1,29 @@
-const { doctors } = require("../data/mockData");
+const doctors = [
+  {
+    id: 1,
+    name: "Dr. Carlos Ramirez",
+    specialty: "Cardiology"
+  },
+  {
+    id: 2,
+    name: "Dr. Ana Lopez",
+    specialty: "Pediatrics"
+  },
+  {
+    id: 3,
+    name: "Dr. Sofia Martinez",
+    specialty: "Dermatology"
+  },
+  {
+    id: 4,
+    name: "Dr. Miguel Torres",
+    specialty: "Neurology"
+  }
+];
 
-function getDoctors(req, res) {
-  return res.status(200).json({
-    message: "Doctor list retrieved successfully",
-    data: doctors
-  });
-}
+const getDoctors = (req, res) => {
+  res.json(doctors);
+};
 
 module.exports = {
   getDoctors
